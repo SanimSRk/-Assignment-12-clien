383dd6cad0f9337847b34980671dafe6a4496dcb
SanimSRk <hasansanim562@gmail.com>
2024-06-13 16:24:11 +0600

    pagination section is done

--- src/Pages/Home/Dashboard/WorkerDashboard/MySubmissions/MySubmissions.jsx
+++ src/Pages/Home/Dashboard/WorkerDashboard/MySubmissions/MySubmissions.jsx
@@ -1,28 +1,58 @@
 import { useQuery } from '@tanstack/react-query';
-import useAxiosPublice from '../../../../../Hooks/AxiosPublic/useAxiosPublice';
 import useAuth from '../../../../../Hooks/useAuth';
 import { BiSolidCoinStack } from 'react-icons/bi';
 import useAxiosSecure from '../../../../../Hooks/AxiosSecure/useAxiosSecure';
+import { useEffect, useState } from 'react';
 
 const MySubmissions = () => {
   const axiosSecure = useAxiosSecure();
   const { user, loding } = useAuth();
+  const [itemPages, setItemsPages] = useState(7);
+  const [curreentPages, setCureentPages] = useState(1);
+  const [counts, setCounts] = useState(0);
+  console.log(counts);
   const { data } = useQuery({
-    queryKey: ['submissions', user?.email],
+    queryKey: ['submissions', user?.email, curreentPages, itemPages],
     enabled: !loding && !!user?.email,
     queryFn: async () => {
       const { data } = await axiosSecure.get(
-        `/my-submission?worker_email=${user?.email}`
+        `/my-submission?worker_email=${user?.email}&page=${
+          curreentPages - 1
+        }&size=${itemPages}`
       );
 
       return data;
     },
   });
 
-  const count = data?.length;
-  const totalPages = 7;
-  const numberOfPages = Math.ceil(count / totalPages);
-  const pages = [...Array(numberOfPages).keys()];
+  useEffect(() => {
+    axiosSecure.get(`/submentCount?email=${user?.email}`).then(res => {
+      setCounts(res.data);
+    });
+  }, []);
+
+  const numberOfPages = Math.ceil(counts?.length / itemPages);
+  let pages = [];
+  for (let i = 0; i < numberOfPages; i++) {
+    pages.push(i + 1);
+  }
+  const handileItemPage = e => {
+    console.log(e.target.value);
+    const vale = parseInt(e.target.value);
+    setItemsPages(vale);
+    setCureentPages(1);
+  };
+  const handileClikPrev = () => {
+    if (curreentPages > 1) {
+      setCureentPages(curreentPages - 1);
+    }
+  };
+  const handileClickNext = () => {
+    if (curreentPages < pages?.length) {
+      setCureentPages(curreentPages + 1);
+    }
+  };
+
   return (
     <div>
       <div className="overflow-x-auto">
@@ -87,6 +117,34 @@ const MySubmissions = () => {
             </tbody>
           ))}
         </table>
+      </div>
+
+      <div className="justify-center flex gap-4 mt-12">
+        <button onClick={handileClikPrev} className="btn ">
+          Prev
+        </button>
+        {pages?.map(item => (
+          <button
+            onClick={() => setCureentPages(item)}
+            className={
+              curreentPages === item
+                ? 'bg-green-500 font-semibold btn'
+                : 'btn font-semibold '
+            }
+            key={item}
+          >
+            {item}{' '}
+          </button>
+        ))}
+        <button onClick={handileClickNext} className="btn">
+          Next
+        </button>
+        <select value={itemPages} onChange={handileItemPage} name="" id="">
+          <option value="5">5</option>
+          <option value="7">7</option>
+          <option value="9">9</option>
+          <option value="11">11</option>
+        </select>
       </div>
     </div>
   );
